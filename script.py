3bb74d69a0b8c8fa785cd3279d06510e964f6be8
Jon Edwards <jedwards@uptopcorp.com>
2016-07-26 19:31:05 -0700

    update script to support Python 3

--- script.py
+++ script.py
@@ -3,16 +3,16 @@
 # Creates a LaunchAgent for local-npm, or does nothing
 # if it's already installed.
 
-import os, sys, getpass, commands
+import os, sys, getpass, subprocess
 
 def command(cmd):
-  print cmd
+  print(cmd)
   os.system(cmd)
 
 def finished():
-  print "\nIf you want to switch back to normal npm, run:\n\n    npmrc default\n"
-  print "If you want to see replication progress, run:\n\n    pm2 logs all\n"
-  print "If you want to uninstall, run:\n\n    launchctl unload ~/Library/LaunchAgents/com.nolanlawson.localnpm.plist\n"
+  print("\nIf you want to switch back to normal npm, run:\n\n    npmrc default\n")
+  print("If you want to see replication progress, run:\n\n    pm2 logs all\n")
+  print("If you want to uninstall, run:\n\n    launchctl unload ~/Library/LaunchAgents/com.nolanlawson.localnpm.plist\n")
 
 install_dir = os.path.expanduser('~/.local-npm')
 if not os.path.exists(install_dir):
@@ -20,11 +20,11 @@ if not os.path.exists(install_dir):
 
 done_file = os.path.join(install_dir, 'done')
 if os.path.exists(done_file):
-  print "\nLooks like local-npm is already installed! Exiting."
+  print("\nLooks like local-npm is already installed! Exiting.")
   finished()
   sys.exit(1)
 
-command('npm install -g local-npm pm2 npmrc')
+command('sudo npm install -g local-npm pm2 npmrc')
 
 plist = '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -57,13 +57,13 @@ plist = '''<?xml version="1.0" encoding="UTF-8"?>
 </plist>'''
 
 plist = plist % (\
-  commands.getoutput('which node'), \
-  commands.getoutput('which pm2'), \
-  commands.getoutput('which node'), \
-  commands.getoutput('which local-npm'), \
+  subprocess.call('which node'.split()), \
+  subprocess.call('which pm2'.split()), \
+  subprocess.call('which node'.split()), \
+  subprocess.call('which local-npm'.split()), \
   getpass.getuser(), \
   install_dir)
-  
+
 out_file = open(os.path.expanduser('~/Library/LaunchAgents/com.nolanlawson.localnpm.plist'), 'w')
 out_file.write(plist)
 out_file.close()
@@ -75,7 +75,7 @@ os.system('npm set registry http://127.0.0.1:5080')
 
 open(done_file, 'a').close()
 
-print "\nDone! You now have local-npm running at 127.0.0.1:5080"
-print "You also have an npmrc called \"local\" configured to point to it."
-print "All offline data is stored in ~/.local-npm"
+print("\nDone! You now have local-npm running at 127.0.0.1:5080")
+print("You also have an npmrc called \"local\" configured to point to it.")
+print("All offline data is stored in ~/.local-npm")
 finished()
